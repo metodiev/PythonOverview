fc8af1f1b796b2c0b7aed070e22541dde8314572
metodiev <miroslav.l.metodiev@gmail.com>
2018-10-28 19:16:16 +0200

    Python Intro
    Python Intro

---
+++ Drawing_a_shape.py
@@ -0,0 +1,9 @@
+character_name = "John"
+character_age = "35"
+
+print(character_name+ " is "  + character_age + "old")
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/_____|")
